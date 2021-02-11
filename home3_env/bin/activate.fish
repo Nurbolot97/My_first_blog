02fa8d6b27445c1380493e5586e925fe50c937eb
Nurba <ndaiyrkanov@gmail.com>
2021-02-12 03:13:16 +0600

    reload

--- home3_env/bin/activate.fish
+++ home3_env/bin/activate.fish
@@ -52,14 +52,14 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(home3_env) "
+        if test -n "(home3_env) "            
             printf "%s%s" "(home3_env) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
             if test $_checkbase = "__"
                 # special case for Aspen magic directories
-                # see https://aspen.io/
+                # see http://www.zetadev.com/software/aspen/
                 printf "%s[%s]%s " (set_color -b blue white) (basename (dirname "$VIRTUAL_ENV")) (set_color normal)
             else
                 printf "%s(%s)%s" (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal)
--- home3_env/bin/django-admin.py
+++ home3_env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/nurbolot/Desktop/Django/Django_home3/home3_env/bin/python3.8
+#!/home/nurbolot/Desktop/Django/Django_home3/home3_env/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
